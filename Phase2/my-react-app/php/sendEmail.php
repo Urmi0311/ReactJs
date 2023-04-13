3a80650eb944bae113dfcf32bf31d21ef4331a63
Urmi0311 <urmi.kothari@sigmainfo.net>
2023-04-13 11:03:42 +0530

    myfirstcommit

--- Phase2/my-react-app/php/sendEmail.php
+++ Phase2/my-react-app/php/sendEmail.php
@@ -26,7 +26,7 @@ if(isset($_POST['fname']) && isset($_POST['email']) && isset($_POST['phone']) &&
         $mail->Host = 'smtp.gmail.com';
         $mail->SMTPAuth = true;
         $mail->Username = 'urmi.kothari@sigmainfo.net';
-        $mail->Password = 'Yashdayama@2808';
+        $mail->Password = '';
         $mail->SMTPSecure = 'tls';
         $mail->Port = 587;
 
--- Phase2/my-react-app/src/components/Footer.js
+++ Phase2/my-react-app/src/components/Footer.js
@@ -1,39 +1,40 @@
 import React from 'react';
 import './Footer.css';
 
+
 function Footer() {
   return (
       <footer className="footer">
           <div className="footer-col">
-              <h3>GET TO KNOW US</h3>
-              <ul>
-                  <li><a href="#">About Us</a></li>
-                  <li><a href="#">Careers</a></li>
-                  <li><a href="#">Press Releases</a></li>
-              </ul>
-          </div>
-          <div className="footer-col">
-              <h3>CONNECT WITH US</h3>
-              <ul>
-                  <li><a href="#">Facebook</a></li>
-                  <li><a href="#">Twitter</a></li>
-                  <li><a href="#">Instagram</a></li>
-              </ul>
-          </div>
-          <div className="footer-col">
-              <h3>CONSUMER POLICY</h3>
-              <ul>
-                  <li><a href="#">Return Policy</a></li>
-                  <li><a href="#">Terms Of Use</a></li>
-                  <li><a href="#">Security</a></li>
-              </ul>
-          </div>
-          <div className="footer-col">
               <h3>HELP</h3>
               <ul>
-                  <li><a href="#">Payments</a></li>
-                  <li><a href="#">Shipping</a></li>
-                  <li><a href="#">Returns</a></li>
+                  <li><a href="#">FAQ</a></li>
+                  <li><a href="#">CONTACT US</a></li>
+                  <li><a href="#">CANCEL ORDER</a></li>
+              </ul>
+          </div>
+          <div className="footer-col">
+              <h3>ACCOUNT</h3>
+              <ul>
+                  <li><a href="#">ACCOUNT</a></li>
+                  <li><a href="#">ORDER HISTORY</a></li>
+                  <li><a href="#">PRIVACY NOTICE</a></li>
+              </ul>
+          </div>
+          <div className="footer-col">
+              <h3>NEED HELP?</h3>
+              <ul>
+                  <li><a href="#">support@lakmeindia.com</a></li>
+                  <li><a href="#">1800-313-9293</a></li>
+                  <li><a href="#">Chat with us on WhatsApp</a></li>
+              </ul>
+          </div>
+          <div className="footer-col">
+              <h3>CUSTOMER SERVICE</h3>
+              <ul>
+                  <li><a href="#">Shipping Policy</a></li>
+                  <li><a href="#">Replacement & Refunds</a></li>
+                  <li><a href="#">Terms & Conditions</a></li>
               </ul>
           </div>
       </footer>
